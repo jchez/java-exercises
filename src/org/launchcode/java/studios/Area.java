3110643cf930afc21cbb65ff9214e98507ff7a1d
Jonathan Sanchez <jonsanchezu@gmail.com>
2017-07-31 20:38:11 -0700

    finished area of a circle studio

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -1,7 +1,43 @@
 package org.launchcode.java.studios;
 
+import java.util.Scanner;
+
+import static java.lang.Math.PI;
+
 public class Area {
+
+    public static float calcArea(float radius) {
+        return (float) (PI * Math.pow(radius,2));
+        //return (float) (3.14 * radius * radius);
+    }
+
     public static void main(String[] args) {
-        System.out.println();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the radius?");
+        float userRadius = input.nextFloat();
+        while(userRadius <= 0) {
+            System.out.println("Invalid radius.");
+            userRadius = input.nextFloat();
+        }
+        float userArea = calcArea(userRadius);
+        System.out.printf("The area is %f for radius of %f", userArea, userRadius);
     }
 }
+
+/*
+do {
+
+}while ();
+*/
+
+/*public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    float userRadius;
+    System.out.println("enter radius");
+    do{
+        userRadius = input.nextFloat();
+        if (userRadius <= 0)
+            System.out.println("Invalid");
+    }while(userRadius <= 0);
+}*/
